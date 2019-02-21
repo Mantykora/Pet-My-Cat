faea70e989609dd2b437c856d893e228a41d456c
Unknown <mantykora@gmail.com>
2019-02-21 21:55:32 +0100

    feat: create text fields in register_route

--- lib/register_route.dart
+++ lib/register_route.dart
@@ -2,23 +2,61 @@ import 'package:flutter/material.dart';
 
 class RegisterRoute extends StatelessWidget {
   String userType;
+
   RegisterRoute(this.userType);
+
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Register as a pet $userType'),
+        backgroundColor: Colors.black54,
       ),
-      body: Center(
-        child: RaisedButton(onPressed: () {
-          Navigator.pop(context);
-        },
-        child: Text('Go back!'),
-        ),
-      )
-    );
+      body: Container(
 
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+
+                decoration: InputDecoration(
+                    labelText: 'Name',
+                    border: OutlineInputBorder(
+
+                    )
+                ),
+              ),
+            ),
+            Padding(
+                padding: const EdgeInsets.all(16.0),
+                child:
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: 'E-mail',
+                      border: OutlineInputBorder(
+
+                      )
+                  ),
+                )
+            ),
+            Padding(
+                padding: const EdgeInsets.all(16.0),
+                child:
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: 'Password',
+                      border: OutlineInputBorder(
+
+                      )
+                  ),
+                )
+            ),
+          ],
+        ),
+
+      ),
+    );
   }
 
 }
