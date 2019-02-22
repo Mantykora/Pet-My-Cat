6743590eaead668ee5635c930344ff39d128ed5e
Unknown <mantykora@gmail.com>
2019-02-22 13:06:11 +0100

    feat: register_route  - add button

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
+        hintColor: Colors.white
       ),
       home: MyHomePage(),
     );
--- lib/register_route.dart
+++ lib/register_route.dart
@@ -8,53 +8,86 @@ class RegisterRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomPadding: false,
       appBar: AppBar(
         title: Text('Register as a pet $userType'),
         backgroundColor: Colors.black54,
       ),
-      body: Container(
+       body: Stack(
+    children: <Widget>[
+    Positioned(
+      top: 0.0,
+        right: 0.0,
+        left: 0.0,
+        height: MediaQuery.of(context).size.height,
+        child:
+        Opacity(
+          opacity: 0.5,
+        child: Image.asset(
+            "assets/blur_background.jpg",
+          fit: BoxFit.fill,
 
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextField(
+        ),
+        ),
+    ),
 
-                decoration: InputDecoration(
-                    labelText: 'Name',
-                    border: OutlineInputBorder(
 
-                    )
+         Padding(
+           padding: const EdgeInsets.all(24.0),
+           child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextField(
+
+                  decoration: InputDecoration(
+                      labelText: 'Name',
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+
+                      )
+                  ),
                 ),
               ),
-            ),
-            Padding(
-                padding: const EdgeInsets.all(16.0),
-                child:
-                TextField(
-                  decoration: InputDecoration(
-                      labelText: 'E-mail',
-                      border: OutlineInputBorder(
+              Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child:
+                  TextField(
+                    decoration: InputDecoration(
+                        labelText: 'E-mail',
+                        border: OutlineInputBorder(
 
-                      )
-                  ),
-                )
-            ),
-            Padding(
-                padding: const EdgeInsets.all(16.0),
-                child:
-                TextField(
-                  decoration: InputDecoration(
-                      labelText: 'Password',
-                      border: OutlineInputBorder(
+                        )
+                    ),
+                  )
+              ),
+              Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child:
+                  TextField(
+                    decoration: InputDecoration(
+                        labelText: 'Password',
+                        border: OutlineInputBorder(
 
-                      )
-                  ),
-                )
-            ),
-          ],
+                        )
+                    ),
+                  )
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all()
+                ),
+                  child: FlatButton(
+                    color: Colors.transparent,
+                    child: Text('Register'),
+                onPressed: showDialog,
+              ),
+              )
+
+            ],
         ),
-
+         ),
+],
       ),
     );
   }
