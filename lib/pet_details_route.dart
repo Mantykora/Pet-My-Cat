8d46b7dbfff1e75f6b80ac826ed6e906fbb85c30
Unknown <mantykora@gmail.com>
2019-02-26 22:30:35 +0100

    feat: pet-detail - add toolbar photo and icons

--- lib/pet_details_route.dart
+++ lib/pet_details_route.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class PetDetailsRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-
       body: NestedScrollView(
         headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
           return <Widget>[
@@ -14,27 +14,45 @@ class PetDetailsRoute extends StatelessWidget {
               floating: false,
               pinned: true,
               flexibleSpace: FlexibleSpaceBar(
-                  centerTitle: true,
-                  title: Text("Collapsing Toolbar",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16.0,
-                      )),
-                  background: Image.network(
-                    "https://images.pexels.com/photos/396547/pexels-photo-396547.jpeg?auto=compress&cs=tinysrgb&h=350",
-                    fit: BoxFit.cover,
-                  )),
+                centerTitle: true,
+                title: Text("Lucy",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0,
+                    )),
+                background:
+                    (Image.asset('assets/cat_photo.jpg', fit: BoxFit.cover)),
 
+                //Image.network(
+//                    "https://images.pexels.com/photos/396547/pexels-photo-396547.jpeg?auto=compress&cs=tinysrgb&h=350",
+//                    fit: BoxFit.cover,
+//                  ),
+              ),
             ),
-
           ];
         },
-        body: Center(
-          child: Text("Sample text"),
+        body: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                SvgPicture.asset('assets/canned-food.svg',
+                    width: 50, height: 50)
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                SvgPicture.asset('assets/litter-box.svg', width: 50, height: 50)
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                SvgPicture.asset('assets/tennis-ball.svg',
+                    width: 50, height: 50)
+              ],
+            )
+          ],
         ),
       ),
     );
   }
-
-
 }
