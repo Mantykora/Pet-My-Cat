3bd799d26bee0b872e5e9e4ea1f3a7ea4c459fbb
Unknown <mantykora@gmail.com>
2019-03-02 18:31:58 +0100

    feat: add date range picker two detail route

--- lib/pet_details_route.dart
+++ lib/pet_details_route.dart
@@ -1,7 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:date_range_picker/date_range_picker.dart' as DateRagePicker;
+import 'package:date_format/date_format.dart';
 
-class PetDetailsRoute extends StatelessWidget {
+class PetDetailsRoute extends StatefulWidget {
+  PetDetailsRoute({Key key}) :super(key: key);
+
+
+
+  @override
+  State<StatefulWidget> createState() => _PetEditState();
+}
+
+class _PetEditState extends State<PetDetailsRoute> {
+  String first;
+  String last;
+
+  Future<Null> _selectDates(BuildContext context) async {
+    final List<DateTime> picked = await DateRagePicker.showDatePicker(
+        context: context,
+        initialFirstDate: new DateTime.now(),
+        initialLastDate: (new DateTime.now()).add(new Duration(days: 7)),
+        firstDate: new DateTime(2015),
+        lastDate: new DateTime(2020)
+    );
+    if (picked != null && picked.length == 2) {
+//      var formatter = new DateFormat.dMMM();
+      setState(() {
+        first = formatDate(picked[0], [dd, '-', mm]);
+        last = formatDate(picked[1], [dd, '-', mm]);
+//        formatDate(picked[0], [d, mm]);
+//        formatDate(picked[1], [d, mm]);
+      });
+
+
+      print(picked[0]);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -22,7 +58,7 @@ class PetDetailsRoute extends StatelessWidget {
                       fontSize: 16.0,
                     )),
                 background:
-                    (Image.asset('assets/cat_photo.jpg', fit: BoxFit.cover)),
+                (Image.asset('assets/cat_photo.jpg', fit: BoxFit.cover)),
 
                 //Image.network(
 //                    "https://images.pexels.com/photos/396547/pexels-photo-396547.jpeg?auto=compress&cs=tinysrgb&h=350",
@@ -34,143 +70,144 @@ class PetDetailsRoute extends StatelessWidget {
         },
         body:
         Column(
-           mainAxisSize: MainAxisSize.min,
+          mainAxisSize: MainAxisSize.min,
 
 
-            children: <Widget>[
+          children: <Widget>[
 
 
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  //  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: SvgPicture.asset('assets/canned-food.svg',
-                          width: 50, height: 50),
-                    ),
-                    Container(
-                      width: 250,
-                      height: 70,
-                      child:
-                      Center(
-                          child: TextField(
-                            maxLines: null,
-                            decoration: InputDecoration(
-                                border: OutlineInputBorder(),
-                                labelText: 'Add food instructions',
-                                fillColor: Colors.white,
-                                filled: true),
-//                  )
-//                ),
-                          )
-                      )
-                      ,
-                    )
-                  ],
-                ),
-              ),
-
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: SvgPicture.asset('assets/litter-box.svg',
-                          width: 50, height: 50),
-                    ),
-                    Container(
-                      width: 250,
-                      height: 70,
-                      child: Center(
-                        child: TextField(
-                          decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: 'Add pet care instructions',
-                              fillColor: Colors.white,
-                              filled: true),
-//                  )
-//                ),
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: SvgPicture.asset('assets/tennis-ball.svg',
-                          width: 50, height: 50),
-                    ),
-                    Container(
-                      width: 250,
-                      height: 70,
-                      child: Center(
-                        child: TextField(
-                          decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: 'Add entertainment instructions',
-                              fillColor: Colors.white,
-                              filled: true),
-//                  )
-//                ),
-                        ),
-                      ),
-                    )
-                    //TextField()
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Container(
-                  width:350,
-
-                  child: Row(
-                    children: <Widget>[
-                      Expanded(
-                          child: TextField(
-                            maxLines: 4,
-                            decoration: InputDecoration(
-
-                              border: OutlineInputBorder(),
-                              labelText: 'Additional notes to your sitter',
-                              fillColor: Colors.white,
-                              filled: true,
-                            ),
-                          ),
-
-                      ),
-                    ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                //  mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SvgPicture.asset('assets/canned-food.svg',
+                        width: 50, height: 50),
                   ),
-                ),
+                  Container(
+                    width: 250,
+                    height: 70,
+                    child:
+                    Center(
+                        child: TextField(
+                          maxLines: null,
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Add food instructions',
+                              fillColor: Colors.white,
+                              filled: true),
+//                  )
+//                ),
+                        )
+                    )
+                    ,
+                  )
+                ],
               ),
+            ),
 
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
 
-                 Padding(
-                   padding: const EdgeInsets.all(8.0),
-                   child: Row(
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          child: SvgPicture.asset('assets/calendar.svg',
-                            width: 50,
-                            height: 50,),
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SvgPicture.asset('assets/litter-box.svg',
+                        width: 50, height: 50),
+                  ),
+                  Container(
+                    width: 250,
+                    height: 70,
+                    child: Center(
+                      child: TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Add pet care instructions',
+                            fillColor: Colors.white,
+                            filled: true),
+//                  )
+//                ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SvgPicture.asset('assets/tennis-ball.svg',
+                        width: 50, height: 50),
+                  ),
+                  Container(
+                    width: 250,
+                    height: 70,
+                    child: Center(
+                      child: TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Add entertainment instructions',
+                            fillColor: Colors.white,
+                            filled: true),
+//                  )
+//                ),
+                      ),
+                    ),
+                  )
+                  //TextField()
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                width:350,
+
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: TextField(
+                        maxLines: 4,
+                        decoration: InputDecoration(
+
+                          border: OutlineInputBorder(),
+                          labelText: 'Additional notes to your sitter',
+                          fillColor: Colors.white,
+                          filled: true,
                         ),
                       ),
-                      Container(
+
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      child: SvgPicture.asset('assets/calendar.svg',
+                        width: 50,
+                        height: 50,),
+                    ),
+                  ),
+                  FlatButton(onPressed:  () => _selectDates(context),
+                      child: Container(
                         width: 250,
                         height: 50,
                         decoration: BoxDecoration(
@@ -185,14 +222,22 @@ class PetDetailsRoute extends StatelessWidget {
 
                           ),
                         ),
-                      )
-                    ],
-                ),
-                 ),
+                      ))
+
+                ],
+              ),
+            ),
+
+            Row(
+              children: <Widget>[
+                Text('${first}'),
+                Text('${last}')
+                    
+              ],
+            )
 
 
-
-            ],
+          ],
 
         ),
 
@@ -200,4 +245,6 @@ class PetDetailsRoute extends StatelessWidget {
       ),
     );
   }
+
+
 }
