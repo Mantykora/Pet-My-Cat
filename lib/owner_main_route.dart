0a929189a331ca5c28aabe4f9b56f378b8fd2d3b
Unknown <mantykora@gmail.com>
2019-03-03 16:38:25 +0100

    feat: add FAB to owner main route

--- lib/owner_main_route.dart
+++ lib/owner_main_route.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:pet_my_cat/pet_details_route.dart';
 
 class OwnerMainRoute extends StatelessWidget {
   @override
@@ -11,17 +12,44 @@ class OwnerMainRoute extends StatelessWidget {
       appBar: AppBar(
         title: Text('Your pets'),
       ),
-      body: ListView(
-    children: <Widget>[
-   ListTile(
-     leading: SvgPicture.asset('assets/cat.svg',
-                width: 50, height: 50,),
-     title: Text('Lucy'),
-     subtitle: Text('Your sitter: Anne'),
-   )
-],
+      body: Stack(
+        children: <Widget>[
+          ListView(
+            children: <Widget>[
+              ListTile(
+                leading: SvgPicture.asset('assets/cat.svg',
+                  width: 50, height: 50,),
+                title: Text('Lucy'),
+                subtitle: Text('Your sitter: Anne'),
+              )
+            ],
 
+          ),
+          Positioned(
+            right: 20.0,
+            bottom: 20,
+            child: Container(
+              width: 70,
+              height: 70,
+              child: FloatingActionButton(
+                foregroundColor: Colors.white,
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              PetDetailsRoute()),
+                    );
+
+                  },
+                child: Icon(Icons.add),
+              ),
+            ),
+          )
+
+        ],
       ),
+
 
     );
 
