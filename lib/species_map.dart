f418d55c08dcb2d8076aa9b857e342230d6f948e
Unknown <mantykora@gmail.com>
2019-03-08 22:05:29 +0100

    feat: add assets

--- lib/species_map.dart
+++ lib/species_map.dart
@@ -4,5 +4,15 @@ import 'package:flutter/material.dart';
 final Map<String, String> speciesMap = {
   'cat': 'assets/pets_photos/cat_photo.jpg',
   'dog': 'assets/pets_photos/dog_photo.jpg',
-  'bird': 'assets/pets_photos/bird_photo.jpg'
+  'bird': 'assets/pets_photos/bird_photo.jpg',
+  'fish': 'assets/pets_photos/fish_photo.jpg',
+  'guinea pig': 'assets/pets_photos/guinea_pig_photo.jpg',
+  'hamster': 'assets/pets_photos/hamster_photo.jpg',
+  'iguana': 'assets/pets_photos/iguana_photo.jpg',
+  'mouse': 'assets/pets_photos/mouse_photo.jpg',
+  'rabbit': 'assets/pets_photos/rabbit_photo.jpg',
+  'snake': 'assets/pets_photos/snake_photo.jpg',
+  'spider': 'assets/pets_photos/spider_photo.jpg',
+  'turtle': 'assets/pets_photos/turtle_photo.jpg',
+
 };
